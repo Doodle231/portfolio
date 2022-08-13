a413280a12ce99ffead9af158a21593716428195
Doodle23 <Doodle23@yahoo.com>
2022-08-13 15:38:13 +0900

    fixed some font issues and styling issue with about me section

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const parallax = document.getElementsByClassName("tech")[0]
+ const activeHam = document.getElementsByClassName("activeham")[0]
+const logo = document.getElementsByClassName("icon")[0]
 
-window.addEventListener("scroll", function(){
-console.log("hey")
+ window.addEventListener("scroll", function(){
 
     let offset = window.scrollY
 parallax.style.backgroundPositionY = offset * .5 + "px"
@@ -32,7 +33,7 @@ const closeHamburgerMenu = document.getElementsByClassName("close")[0]
 
 closeHamburgerMenu.addEventListener("click", function(){
 
-   const activeHam = document.getElementsByClassName("activeham")[0]
+   logo.style.display = "block"
    activeHam.style.display = "none"
 })
 
@@ -43,4 +44,35 @@ OpenHamburgerMenu.addEventListener("click", function(){
 
     const activeHam = document.getElementsByClassName("activeham")[0]
     activeHam.style.display = "block"
+    logo.style.display = "none"
  })
+
+
+ const HamburgerMenuLinks = document.getElementsByClassName("hamclass")
+
+
+ for (let i = 0; i < HamburgerMenuLinks.length; i++) {
+   
+    HamburgerMenuLinks[i].addEventListener("click", function(){
+
+        activeHam.style.display = "none"
+        logo.style.display = ""
+        })
+      
+
+  } 
+
+
+ let flashcardapp = document.getElementById("liveflashcard")
+
+  flashcardapp.addEventListener("click", function(){
+
+    window.location.href = "https://danroyals.com/flashcard.html";
+  })
+
+  let attendanceApp = document.getElementById("liveattendance")
+
+  attendanceApp.addEventListener("click", function(){
+
+    window.location.href = "https://danroyals.com/attendancetracker.html";
+  })
