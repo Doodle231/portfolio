ec4f01c1143ba408cb1eae028f226d1b5bfdec48
Doodle23 <Doodle23@yahoo.com>
2022-10-29 12:34:31 +0900

    fixed some wonky paragraph widths on mobile

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 let hamicon = document.getElementById("hamburgericon")
 let menu = document.getElementById("mobilenavmenu")
-let mobileLinks = document.getElementById("mobilelinks")
-
+let mobileLinks = document.getElementsByClassName("mobilelink")
 
 hamicon.addEventListener("click", function() {
     menu.style.display = "flex"
    
-   mobileLinks.style.display = "block"
-    menu.classList.toggle("active")
-    mobileLinks.classList.toggle("active")
 
+    menu.classList.toggle("active")
+    console.log(mobileLinks[0])
+    mobileLinks[0].classList.toggle("active")
+    mobileLinks[1].classList.toggle("active")
+    mobileLinks[2].classList.toggle("active")
+    mobileLinks[3].classList.toggle("active")
+
+    
   });
 
 
 
+
+
+    for (let i = 0; i < mobileLinks.length; i++) {
+        mobileLinks[i].addEventListener("click", function() {
+
+            menu.style.display = "none"
+      })
+
+    }
